0ebdef4d0ec188f44ba4ede6c1cafe9d4971cbca
DangerousDB <100533044+DangerousDB@users.noreply.github.com>
2025-05-27 22:17:30 -0700

    Added Cloud Global Variables and "Shading" levels

--- main.cpp
+++ main.cpp
@@ -16,6 +16,11 @@ const int HEIGHT = pow(2, SCALE);
 const int NUM_FRAMES = 120;
 const int NUM_GIFS = 1;
 const int RADIUS = pow(2, SCALE - 2);
+const int SHADING_LEVELS = -1; // -1 for smooth shading
+
+// Global Random Variables
+double CLOUD_PROBABILTIY;
+double CLOUD_DENSITY;
 
 std::vector<double> generate_normal_map(int r)
 {
@@ -113,18 +118,26 @@ double get_cloud_value(const SimplexNoise *s, double x, double y, double z)
     terrain_value += 0.125 * (simplex.noise(x * 8, y * 8, z * 8) + 1) / 2;
     terrain_value += 0.125 * (simplex.noise(x * 16, y * 16, z * 16) + 1) / 2;
     return terrain_value;
+}
 
+// Choose if Global Random Variables are random and if now what value they are
+void init_rand_global_variables()
+{
+    CLOUD_PROBABILTIY = true ? double(rand()) / RAND_MAX : 0.4;
+    CLOUD_DENSITY = true ? 2 * double(rand()) / RAND_MAX : 1.5;
 }
 
 // Generates a simple animation: a color-shifting square
 void generate_gif(const std::string &filename)
 {
+    // Randomized scalers used to ensure randomness of generation
     int simplexScalerX = rand() % WIDTH - (WIDTH / 2);
     int simplexScalerY = rand() % WIDTH - (WIDTH / 2);
     int simplexScalerZ = rand() % WIDTH - (WIDTH / 2);
 
     const SimplexNoise simplex(0.01f, 0.5f, 3.0f, 0.5f);
     std::vector<unsigned char> frame(WIDTH * HEIGHT * 4); // RGBA
+    // precalculate normal maps for planet and cloud to save on calculation time
     std::vector<double> planet_normal_map = generate_normal_map(int(double(RADIUS) * .95));
     std::vector<double> cloud_normal_map = generate_normal_map(RADIUS);
 
@@ -145,6 +158,10 @@ void generate_gif(const std::string &filename)
             for (int y = -RADIUS; y < RADIUS; y++)
             {
 
+                // since sin & cos go back and forth between 1 & -1 we can use these to smoothly loop overtime
+                double cos_loop = std::cos(double(frame_idx) * 2 / double(NUM_FRAMES) * M_PI);
+                double sin_loop = std::sin(double(frame_idx) * 2 / double(NUM_FRAMES) * M_PI);
+
                 // calculate where the sphere pixels will be on the actual image and the corresponding index for the array
                 double px = x + midw;
                 double py = y + midh;
@@ -160,13 +177,13 @@ void generate_gif(const std::string &filename)
                 double cloud_ny = cloud_normal_map[nidx + 1];
                 double cloud_nz = cloud_normal_map[nidx + 2];
 
-                // if we get a weird value thats the map telling us to skip this point
+                // if the normal vector is <-1 we are outside of the cloud radius and planet radius (since cloud > planet) so just skip the pixel
                 if (cloud_nx < -1)
                 {
                     continue;
                 }
 
-                // make our actaul vector and normalize it just in case
+                // make our actual vector and normalize it just in case
                 Eigen::Vector3d planet_normal_vec(planet_nx, planet_ny, planet_nz);
                 Eigen::Vector3d cloud_normal_vec(cloud_nx, cloud_ny, cloud_nz);
                 planet_normal_vec.normalize();
@@ -182,22 +199,33 @@ void generate_gif(const std::string &filename)
                 // Eigen::Vector3d planet_biased_normal_vec(0.5 * (planet_rotated_normal_vec[0] + 1), 0.5 * (planet_rotated_normal_vec[1] + 1), planet_rotated_normal_vec[2]);
 
                 // light calculations
-                double light_intensity = 0.5;
-                Eigen::Vector3d light_direction(std::cos(frame_idx * 4 / double(NUM_FRAMES) * M_PI), 0, std::sin(frame_idx * 4 / double(NUM_FRAMES) * M_PI));
+                double light_intensity = 10;
+                Eigen::Vector3d light_direction(std::cos(double(frame_idx) * 4 / double(NUM_FRAMES) * M_PI) * 2, 0, std::sin(double(frame_idx) * 4 / double(NUM_FRAMES) * M_PI) * 2);
                 light_direction.normalize();
                 double light_power = planet_normal_vec.dot(light_direction) * light_intensity;
-                double diffuse = (0 < light_power) ? light_power : 0;
+                double diffuse = (0 < light_power) ? (light_power > light_intensity) ? light_intensity : light_power : 0;
+                diffuse *= 0.8 / light_intensity;
+                if (SHADING_LEVELS != -1)
+                {
+                    double shading_step_size = 0.8 / (SHADING_LEVELS - 1);
+                    diffuse = double(int((diffuse + shading_step_size / 2) / shading_step_size));
+                    diffuse = diffuse * shading_step_size;
+                }
                 double ambient = 0.2f;
+                double shade = diffuse + ambient;
 
+                // calculate the probability that this is a cloud using random scaling and cyclic noise so that the clouds move in smooth way and are random
                 double cloud_val = get_cloud_value(&simplex,
-                                                   cloud_rotated_normal_vec[0] + simplexScalerX - std::cos(double(frame_idx) * 2 / double(NUM_FRAMES) * M_PI),
-                                                   cloud_rotated_normal_vec[1] + simplexScalerY - std::sin(double(frame_idx) * 2 / double(NUM_FRAMES) * M_PI),
-                                                   cloud_rotated_normal_vec[2] + simplexScalerZ - std::sin(double(frame_idx) * 2 / double(NUM_FRAMES) * M_PI));
-                double is_cloud = cloud_val >= 0.4 + (std::sin(double(frame_idx) * 2 / double(NUM_FRAMES) * M_PI) / 10);
+                                                   cloud_rotated_normal_vec[0] + simplexScalerX - cos_loop,
+                                                   cloud_rotated_normal_vec[1] + simplexScalerY - sin_loop,
+                                                   cloud_rotated_normal_vec[2] + simplexScalerZ - sin_loop);
 
-                std::vector<int>
-                    terrain_color(3);
+                // determine if this point is a cloud, using some cyclic noise so the size of the clouds grow and shrink
+                double is_cloud = cloud_val >= CLOUD_PROBABILTIY + (sin_loop / 10);
 
+                std::vector<int> terrain_color(3);
+
+                // If the x value of the normal vector is less than -1 (not normalized) we must be outside of the planet's radius
                 if (!(planet_nx < -1))
                 {
                     terrain_color = get_terrain_value(&simplex,
@@ -206,10 +234,12 @@ void generate_gif(const std::string &filename)
                                                       planet_rotated_normal_vec[2] - simplexScalerZ);
                 }
 
-                // opacity formula for adding clouds C2 is the transparent color being added to C1
+                // calculate how thick the clouds should be at this point
+                double cloud_opacity = (1 < cloud_val * CLOUD_DENSITY) ? 1 : (0 > cloud_val * CLOUD_DENSITY) ? 0
+                                                                                                             : cloud_val * CLOUD_DENSITY;
+
+                // if this pixel is determined to have a cloud on it then draw it as so
                 // ( (1-p)R1 + p*R2, (1-p)*G1 + p*G2, (1-p)*B1 + p*B2 )
-                double cloud_opacity = (1 < cloud_val * 1.5) ? 1 : (0 > cloud_val * 1.5) ? 0
-                                                                                         : cloud_val * 1.5;
                 if (is_cloud)
                 {
                     terrain_color[0] = (1 - cloud_opacity) * terrain_color[0] + cloud_opacity * 255;
@@ -217,10 +247,10 @@ void generate_gif(const std::string &filename)
                     terrain_color[2] = (1 - cloud_opacity) * terrain_color[2] + cloud_opacity * 255;
                 }
 
-                frame[idx + 0] = (diffuse + ambient) * terrain_color[0]; // R
-                frame[idx + 1] = (diffuse + ambient) * terrain_color[1]; // G
-                frame[idx + 2] = (diffuse + ambient) * terrain_color[2]; // B
-                frame[idx + 3] = 255;                                    // A
+                frame[idx + 0] = shade * terrain_color[0]; // R
+                frame[idx + 1] = shade * terrain_color[1]; // G
+                frame[idx + 2] = shade * terrain_color[2]; // B
+                frame[idx + 3] = 255;                      // A
             }
         }
 
@@ -238,6 +268,7 @@ void generate_gif(const std::string &filename)
 int main()
 {
     std::srand(std::time(0));
+    init_rand_global_variables();
 
     for (int i = 0; i < NUM_GIFS; ++i)
     {
